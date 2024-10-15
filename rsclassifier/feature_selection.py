931887aa45786fc14f93c8e6de414d4fe1a89260
ReijoJaakkola <jaakkolareijo@hotmail.com>
2024-10-15 16:59:53 +0300

    Types and documentation to feature_selection

--- rsclassifier/feature_selection.py
+++ rsclassifier/feature_selection.py
@@ -1,7 +1,23 @@
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-def feature_selection_using_random_forest(X, y, k):
+def feature_selection_using_random_forest(X : pd.DataFrame, y : pd.Series, k : int) -> list:
+    """
+    Perform feature selection using a Random Forest classifier.
+
+    This function trains a Random Forest classifier on the given dataset and ranks the features 
+    based on their importance as determined by the classifier. It then returns the top `k` most 
+    important features.
+
+    Args:
+        X (pd.DataFrame) : The feature data.
+        y (pandas.Series): The target labels.
+        k (int): The number of top-ranked features to return.
+
+    Returns:
+        list: A list of the top `k` most important feature names.
+    """
     rfc = RandomForestClassifier(random_state = 42)
     rfc.fit(X, y)
     importances = rfc.feature_importances_
@@ -12,7 +28,22 @@ def feature_selection_using_random_forest(X, y, k):
     ranked_features = list((dict(sorted(feature_importances.items(), key=lambda item: item[1], reverse = True)).keys()))
     return ranked_features[:k]
 
-def feature_selection_using_decision_tree(X, y, k):
+def feature_selection_using_decision_tree(X : pd.DataFrame, y : pd.Series, k : int) -> list:
+    """
+    Perform feature selection using a Decision Tree classifier.
+
+    This function trains a Decision Tree classifier on the given dataset and ranks the features 
+    based on their importance as determined by the classifier. It then returns the top `k` most 
+    important features.
+
+    Args:
+        X (pd.DataFrame) : The feature data.
+        y (pandas.Series): The target labels.
+        k (int): The number of top-ranked features to return.
+
+    Returns:
+        list: A list of the top `k` most important feature names.
+    """
     dt = DecisionTreeClassifier(random_state = 42)
     dt.fit(X, y)
     importances = dt.feature_importances_
